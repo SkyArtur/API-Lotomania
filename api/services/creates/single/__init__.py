44432289ebb718fe0a453010123ba74e297f7007
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-06-08 21:10:58 -0300

    fix: remove deprecated create_bet_prize function

--- api/services/creates/single/__init__.py
+++ api/services/creates/single/__init__.py
@@ -1,6 +1,5 @@
 from .create_contest import create_contest
 from .create_bet import create_bet
-from .create_bet_prize import create_bet_prize
 
 
-__all__ = ['create_contest', 'create_bet', 'create_bet_prize']
+__all__ = ['create_contest', 'create_bet']
