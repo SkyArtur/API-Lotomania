b3c45258ee45eeb74f3be26dd7891a74112a0fff
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-08-08 18:27:54 -0300

    chore: define os imports em "core/utils"

---
+++ core/utils/__init__.py
@@ -0,0 +1,20 @@
+from .readers import leitor_csv
+from .parsers import analisar_numeros, analisar_pontos, analisar_datas, analisar_sorteio
+from .extractors import (
+    extrair_numeros_sorteio_lotomania,
+    extrair_referencias_sorteio_lotomania,
+    extrair_premios_sorteio_lotomania
+)
+
+
+
+__all__ = [
+    'analisar_numeros',
+    'analisar_pontos',
+    'analisar_datas',
+    'analisar_sorteio',
+    'leitor_csv',
+    'extrair_numeros_sorteio_lotomania',
+    'extrair_referencias_sorteio_lotomania',
+    'extrair_premios_sorteio_lotomania'
+]
