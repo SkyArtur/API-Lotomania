66e0614f9060ac29f320315bef5c9187a3e74887
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-08-10 00:26:56 -0300

    feat: registradas as rotas da API em api/urls.py

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,9 @@ app_name = 'api'
 
 router = DefaultRouter()
 
+router.register('sorteios', views.SorteioViewSet, basename='sorteios')
+router.register('apostas', views.ApostaViewSet, basename='apostas')
+router.register('numeros', views.NumeroViewSet, basename='numeros')
+router.register('apostador', views.ApostadorViewSet, basename='apostador')
+
 urlpatterns = router.urls
