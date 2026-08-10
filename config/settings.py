167470c685c1ed0205df5aeaa641833c8fb25887
Artur dos Santos Shon <sky_artur@hotmail.com>
2026-08-10 03:00:27 -0300

    chore: ajustar versao em SPECTACULAR_SETTINGS para 0.1.0

--- config/settings.py
+++ config/settings.py
@@ -187,7 +187,7 @@ MAILERS = {
 SPECTACULAR_SETTINGS = {
     'TITLE': 'API-Lotomania',
     'DESCRIPTION': 'API para consulta de dados da Lotomania',
-    'VERSION': '1.0.0',
+    'VERSION': '0.1.0',
     'SERVE_INCLUDE_SCHEMA': False,
 }
 
